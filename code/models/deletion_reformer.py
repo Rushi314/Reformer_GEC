1f833e045be79a8781453a9b28a437b0c3be8f9b
Shubham Shetty <79488655+shubham-shetty@users.noreply.github.com>
2021-12-12 17:36:17 -0500

    Updated LR

--- code/models/deletion_reformer.py
+++ code/models/deletion_reformer.py
@@ -57,7 +57,7 @@ class DeletionReformer(pl.LightningModule):
 
     # Tells PyTorch Lightning which optimizer to use
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
         return optimizer
 
     # Collate function for PyTorch DataLoader
